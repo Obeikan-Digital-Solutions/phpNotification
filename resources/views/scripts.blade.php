7088fae6fd746119c59e3a14121bc8771e1fe4f4
Osama Al-mamari <yemencoder@gmail.com>
2023-08-29 10:06:42 +0300

    Create scripts.blade.php

---
+++ resources/views/scripts.blade.php
@@ -0,0 +1,11 @@
+<script>
+    function sendMarkRequest(id = null) {
+        return $.ajax("{{ route('mark.notification') }}", {
+            method: "POST",
+            data: {
+                _token: "{{ csrf_token() }}",
+                id
+            }
+        });
+    }
+</script>
